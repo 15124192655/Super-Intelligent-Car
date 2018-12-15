a13d8becc0e6e4eb4b92e14695728652adf5cab4
15124192655 <44692664+15124192655@users.noreply.github.com>
2018-12-15 11:22:19 +0000

    Add files via upload

---
+++ weixin/app.js
@@ -0,0 +1,40 @@
+App({
+
+  onLaunch() {
+
+    console.log('App.onLaunch()');
+
+  },
+
+  onShow: function () {
+
+  },
+
+  onHide: function () {
+
+  },
+
+
+
+  //本应用全局数据
+
+  globalData: {
+
+    temperature: {},
+
+    light: {},
+
+    humidity: {},
+   
+
+    userInfo: null,
+
+    postURL: null,
+
+    cameraURL: null
+
+    
+
+  }
+
+})
